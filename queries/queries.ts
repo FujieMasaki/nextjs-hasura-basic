f9e1b5a4805eb0c645f0229acb4f03f808362df0
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2022-04-30 10:02:51 +0900

    dsファイル

--- queries/queries.ts
+++ queries/queries.ts
@@ -1,11 +1,12 @@
+// フロントエンドでコマンドとしてクエリを作成
 import { gql } from '@apollo/client'
 
 export const GET_USERS = gql`
   query GetUsers {
-    users(order_by: { create_at: desc }) @client {
+    users(order_by: { created_at: desc }){
       id
       name
-      create_at
+      created_at
     }
   }
 `
@@ -19,30 +20,50 @@ export const GET_USERS_LOCAL = gql`
     }
   }
 `
+// @clientはgrafhqlのクライアントサイドのキャッシュを見に行く。→ユーザーの一覧情報へ好きにアクセスできる
 
 export const GET_USERIDS = gql`
   query GetUsersIds {
-    users(order_by: { create_at: desc }) {
+    users(order_by: { created_at: desc }) {
       id
-      name
-      create_at
     }
   }
 `
 
 export const GET_USERBY_ID = gql`
-  query GetUserById($id: uuid) {
+  query GetUserById($id: uuid!) {
     users_by_pk(id: $id) {
       id
       name
-      create_at
+      created_at
     }
   }
 `
+//  idに対応した値を一つだけ取得してくる
 
 export const CREATE_USER = gql`
- mutation CreateUser($name: $n){
+ mutation CreateUser($name: String!){
   insert_users_one(object: {name:$name}){
+  id
+  name
+  created_at
+}
+}
+`
+
+export const DELETE_USER = gql`
+ mutation DeleteUser($id: uuid!){
+  delete_users_by_pk(id: $id){
+  id
+  name
+  created_at
+}
+}
+`
+
+export const UPDATE_USER = gql`
+ mutation UpdateUser($id: uuid!, $name: String!){
+  update_users_by_pk(pk_columns:{ id : $id }, _set:{ name:$name }){
   id
   name
   created_at
